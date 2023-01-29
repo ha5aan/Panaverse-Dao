5433c7656cc2d77517603901f8d62b2dbeac3382
Muhammad Hasaan <m.hasaan1999@gmail.com>
2023-01-29 12:05:31 +0500

    after build

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,10 +16,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
     
-    {/* <HomePicture/> */}
-    {/* <CourseContent/> */}
-    {/* <Outcomes/> */}
-    {/* <RelevantLinks/> */}
+    <HomePicture/>
+    <CourseContent/>
+    <Outcomes/>
+    <RelevantLinks/>
    
     </>
   )
