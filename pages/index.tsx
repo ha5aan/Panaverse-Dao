008eedbd1316809dbba66258a0d9912ea5c6347e
Muhammad Hasaan <m.hasaan1999@gmail.com>
2023-01-29 11:58:14 +0500

    see problem

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,8 +22,8 @@ export default function Home() {
     
     <HomePicture/>
     <CourseContent/>
-    <Outcomes/>
-    {/* <RelevantLinks/> */}
+    {/* <Outcomes/> */}
+    <RelevantLinks/>
    
     </>
   )
