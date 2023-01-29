3fe7c13e3730ea9ec9e91dcc292a09334d67599f
Muhammad Hasaan <m.hasaan1999@gmail.com>
2023-01-29 15:36:31 +0500

    removed relevant material

--- pages/Components/HomePicture.tsx
+++ pages/Components/HomePicture.tsx
@@ -5,7 +5,7 @@ import { Slide } from "react-awesome-reveal";
 import {Box, Flex, Heading} from '@chakra-ui/react'
 const HomePicture = () => {
   return (
-    <Box  h={['calc(30vh)','calc(30vh)','calc(50vh)','calc(80vh)']} className={styles.backgroundImageSet} >
+    <Box  className={styles.backgroundImageSet} >
         <Flex justifyContent={"space-between"}>
                 <Box className={styles.mainHeadingSet}  >
         <Heading fontSize={["xs","xs","xl","4xl","5xl"]}>
