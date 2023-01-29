c1d055755000ad7494d17c3010e4508c6056fdf5
Muhammad Hasaan <m.hasaan1999@gmail.com>
2023-01-29 14:00:15 +0500

    removed homepage picture

--- pages/Components/HomePicture.tsx
+++ pages/Components/HomePicture.tsx
@@ -3,25 +3,32 @@ import styles from "../../styles/HomeTitle.module.css"
 import { Slide } from "react-awesome-reveal";
 
 import {Box, Flex, Heading} from '@chakra-ui/react'
+// const HomePicture = () => {
+//   return (
+//     <Box  h={['calc(30vh)','calc(30vh)','calc(50vh)','calc(80vh)']} className={styles.backgroundImageSet} >
+//         <Flex justifyContent={"space-between"}>
+//                 <Box className={styles.mainHeadingSet}  >
+//         <Heading fontSize={["xs","xs","xl","4xl","5xl"]}>
+// <Slide> Certified Web3 and Metaverse Developer </Slide>
+//         </Heading>
+//         </Box>
+
+
+
+//         <Box className={styles.mainHeadingSet}  >
+//         <Heading fontSize={["xs","xs","xl","4xl","5xl"]}>
+//        <Slide direction='right'> In Karachi, Lahore, Islamabad, and Peshawar </Slide>       </Heading>
+//         </Box>
+//         </Flex>
+        
+//      </Box>
+//   )
+// }
+
+
 const HomePicture = () => {
   return (
-    <Box  h={['calc(30vh)','calc(30vh)','calc(50vh)','calc(80vh)']} className={styles.backgroundImageSet} >
-        <Flex justifyContent={"space-between"}>
-                <Box className={styles.mainHeadingSet}  >
-        <Heading fontSize={["xs","xs","xl","4xl","5xl"]}>
-<Slide> Certified Web3 and Metaverse Developer </Slide>
-        </Heading>
-        </Box>
-
-
-
-        <Box className={styles.mainHeadingSet}  >
-        <Heading fontSize={["xs","xs","xl","4xl","5xl"]}>
-       <Slide direction='right'> In Karachi, Lahore, Islamabad, and Peshawar </Slide>       </Heading>
-        </Box>
-        </Flex>
-        
-     </Box>
+    <div>HomePicture</div>
   )
 }
 
