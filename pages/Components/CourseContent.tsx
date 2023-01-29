2f7269bfaaed87e1bb1330c7f7de23f41c21f86e
Muhammad Hasaan <m.hasaan1999@gmail.com>
2023-01-29 14:53:11 +0500

    removed relevant material

--- pages/Components/CourseContent.tsx
+++ pages/Components/CourseContent.tsx
@@ -6,10 +6,10 @@ const CourseContent = () => {
   return (
     <Box>
      
-<Heading fontSize={"5xl"} className={styles.courseHeading}  mb={"2%"} mt={"1%"}><Fade> The Program in Nutshell</Fade></Heading>
+<Heading fontSize={"5xl"} className={styles.courseHeading}  mb={"2%"} mt={"1%"}><Fade  triggerOnce={true}> The Program in Nutshell</Fade></Heading>
         {/* </Container> */}
         <Container maxWidth={"4xl"} fontSize="xl" fontWeight={"medium"}>
-      <Fade> In this brand-new type of curriculum, students will learn how to make money and boost exports in the classroom and will begin doing so within six months of the programs beginning. It resembles a cross between a corporate venture and an educational project.</Fade> 
+      <Fade  triggerOnce={true}> In this brand-new type of curriculum, students will learn how to make money and boost exports in the classroom and will begin doing so within six months of the programs beginning. It resembles a cross between a corporate venture and an educational project.</Fade> 
         </Container>
         <Flex mx={"10%"} mt={"2%"} justifyContent={"space-around"} wrap={"wrap"}>
             <Box mx={"2%"} my={"1%"} >
--- pages/Components/Outcomes.tsx
+++ pages/Components/Outcomes.tsx
@@ -14,12 +14,12 @@ const Outcomes = () => {
         <Container mb={"2%"} textAlign={"center"}>
              <Heading> Outcomes </Heading>
         </Container>
-        <Fade>
+        <Fade triggerOnce={true}>
    <Container  maxWidth={"4xl"} fontSize="md" fontWeight={"medium"}>
    The graduates of this program will own products (Full-Stack App Templates, AR and VR Experiences, and APIs) that are marketed globally by the Panaverse DAO and, if they like, will also be able to start off by offering services at a rate of <Text as={"b"} > $50 </Text>per hour <Text as={"b"}> ($96,000 per year) </Text>. This would give Pakistani professionals and students a fantastic opportunity to better their financial situation while also giving the economy a much-needed boost by expanding software exports.
    </Container>
    </Fade>
-   <Fade>
+   <Fade  triggerOnce={true}>
    <Container  maxWidth={"4xl"} mt={"2%"}>
    <List spacing={3}>
   <ListItem>
