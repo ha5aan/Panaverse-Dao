ccfb687cb45c7963cb5e26a626f7a8cebe183b85
Muhammad Hasaan <m.hasaan1999@gmail.com>
2023-01-29 15:02:25 +0500

    removed relevant material

--- pages/Course.tsx
+++ pages/Course.tsx
@@ -1,12 +1,12 @@
 import { Accordion, Text,AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Container, Heading, List, ListItem, OrderedList, UnorderedList } from "@chakra-ui/react";
 import Link from "next/link";
 import { Fade } from "react-awesome-reveal";
-import styles from "../styles/Course.module.css"
+//import styles from "../styles/Course.module.css"
 
 export default function Home() {
     return (
       <>
-      <Box className={styles.adjustingFooter}>
+      <Box >
       <Container textAlign={"center"} mt={"2%"}>
       <Heading> <Fade> Detailed Course Syllabus</Fade></Heading>
       </Container>
