571bf1808a7196843e893b0be28d56da128cd121
Muhammad Hasaan <m.hasaan1999@gmail.com>
2023-01-30 22:43:50 +0500

    flex

--- pages/Components/HomePicture.tsx
+++ pages/Components/HomePicture.tsx
@@ -6,7 +6,7 @@ import {Box, Flex, Heading} from '@chakra-ui/react'
 const HomePicture = () => {
   return (
     <Box  h={['calc(30vh)','calc(30vh)','calc(50vh)','calc(80vh)']} className={styles.backgroundImageSet} >
-         {/* <Flex justifyContent={"space-between"}> */}
+         <Flex justifyContent={"space-between"}>
                 <Box className={styles.mainHeadingSet}  >
         <Heading fontSize={["xs","xs","xl","4xl","5xl"]}>
 <> Certified Web3 and Metaverse Developer </>
@@ -18,8 +18,8 @@ const HomePicture = () => {
         <Box className={styles.mainHeadingSet}  >
         <Heading fontSize={["xs","xs","xl","4xl","5xl"]}>
        < > In Karachi, Lahore, Islamabad, and Peshawar </>       </Heading>
-        </Box>
-        </Flex> */}
+        </Box>*/}
+        </Flex> 
         
      </Box>
   )
