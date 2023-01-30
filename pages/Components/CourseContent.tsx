75c4cdd35e6d596915ea3c234e9987680fad48db
Muhammad Hasaan <m.hasaan1999@gmail.com>
2023-01-30 22:59:41 +0500

    content without fade of course

--- pages/Components/CourseContent.tsx
+++ pages/Components/CourseContent.tsx
@@ -6,10 +6,10 @@ const CourseContent = () => {
   return (
     <Box>
      
-<Heading fontSize={"5xl"} className={styles.courseHeading}  mb={"2%"} mt={"1%"}><Fade  triggerOnce={true}> The Program in Nutshell</Fade></Heading>
+<Heading fontSize={"5xl"} className={styles.courseHeading}  mb={"2%"} mt={"1%"}><> The Program in Nutshell</></Heading>
         {/* </Container> */}
         <Container maxWidth={"4xl"} fontSize="xl" fontWeight={"medium"}>
-      <Fade  triggerOnce={true}> In this brand-new type of curriculum, students will learn how to make money and boost exports in the classroom and will begin doing so within six months of the programs beginning. It resembles a cross between a corporate venture and an educational project.</Fade> 
+      < > In this brand-new type of curriculum, students will learn how to make money and boost exports in the classroom and will begin doing so within six months of the programs beginning. It resembles a cross between a corporate venture and an educational project.</> 
         </Container>
 
     </Box>
