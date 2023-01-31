406ae7a414ba91e22eff61719f3bedc932eddc4b
Muhammad Hasaan <m.hasaan1999@gmail.com>
2023-01-31 20:50:14 +0500

    added slide outside heading

--- pages/Components/HomePicture.tsx
+++ pages/Components/HomePicture.tsx
@@ -8,9 +8,9 @@ const HomePicture = () => {
     <Box  h={['calc(30vh)','calc(30vh)','calc(50vh)','calc(80vh)']} className={styles.backgroundImageSet} >
          <Flex justifyContent={"space-between"}>
                 <Box className={styles.mainHeadingSet}  >
-        <Heading fontSize={["xs","xs","xl","4xl","5xl"]}>
+        <Slide> <Heading fontSize={["xs","xs","xl","4xl","5xl"]}>
 <> Certified Web3 and Metaverse Developer </>
-        </Heading>
+        </Heading> </Slide>
         </Box>
 
 
