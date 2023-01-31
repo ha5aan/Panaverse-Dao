c036e7bc01707a69bfe6c21551876e7bcca1187b
Muhammad Hasaan <m.hasaan1999@gmail.com>
2023-01-31 21:34:29 +0500

    overflow hiidden

--- pages/Components/HomePicture.tsx
+++ pages/Components/HomePicture.tsx
@@ -5,7 +5,7 @@ import { Slide } from "react-awesome-reveal";
 import {Box, Flex, Heading} from '@chakra-ui/react'
 const HomePicture = () => {
   return (
-    <Box  h={['calc(30vh)','calc(30vh)','calc(50vh)','calc(80vh)']} className={styles.backgroundImageSet} >
+    <Box  h={['calc(30vh)','calc(30vh)','calc(50vh)','calc(80vh)']} className={styles.backgroundImageSet}  overflow="hidden">
          <Flex justifyContent={"space-between"}>
                 <Box className={styles.mainHeadingSet}  >
         <Slide> <Heading fontSize={["xs","xs","xl","4xl","5xl"]}>
